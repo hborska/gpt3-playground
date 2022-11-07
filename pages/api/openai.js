68fe302302a1c6d710f2ce3c98441d08cb45f152
Henry Borska <henryborska@Henrys-MacBook-Pro-2.local>
2022-11-07 11:34:16 -0600

    redeploy

--- pages/api/openai.js
+++ pages/api/openai.js
@@ -12,7 +12,6 @@ export default async function (req, res) {
     temperature: 0.6,
   });
 
-  // console.log(completion.data.choices[0].text);
   res.status(200).json({ result: completion.data.choices[0].text });
 }
 
